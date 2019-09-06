7f2c4272787f298444959e0c055d7ccf21bc957e
Denis <blackpirates@mail.ru>
2019-09-06 22:13:43 +0300

    Последние правки

--- source/js/app.js
+++ source/js/app.js
@@ -1,8 +1,11 @@
 window.onload = function () {
   const menuButton = document.querySelector(".hamburger-button");
   const navigation = document.querySelector(".navigation");
+  const map = document.querySelector(".map");
 
-  navigation.classList.add("navigation_collapsed");
+  navigation.classList.remove("navigation__no-js");
+  menuButton.classList.remove("hamburger-button__no-js");
+  map.classList.remove("map__no-js");
 
   menuButton.addEventListener("click", function(e) {
     e.preventDefault();
